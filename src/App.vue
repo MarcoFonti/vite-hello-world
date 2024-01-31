6487ecffe2f95dc742b217c7df7b6c2ed04e850e
MarcoFonti <theemporiumg@gmail.com>
2024-01-31 16:13:27 +0100

    add scaffolding

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<!-- JS -->
+<script>
+export default{
+  
+};
+</script>
+
+<!-- HTML -->
+<template>
+  
+</template>
+
+<!-- CSS -->
+<style>
+
+</style>
+
---
+++ src/main.js
@@ -0,0 +1,7 @@
+// PRENDIMI VUE DA NODE_MODULES
+import { createApp } from 'vue'
+// IMPORTA APP.VUE
+import App from './App.vue'
+
+// PASSIMI APP
+createApp(App).mount('#app')
