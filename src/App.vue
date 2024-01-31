3b31ca744fda5ad45c8b308bf94ee5da265b4da9
MarcoFonti <theemporiumg@gmail.com>
2024-01-31 16:25:44 +0100

    add AppHeader

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,23 @@
 <!-- JS -->
 <script>
+// ! IMPORTO APPHEADER
+import AppHeader from './components/AppTitle.vue'
+
 export default{
-  
+    name: 'MyApp',
+    data: () => ({
+
+    }),
+    components: {
+        AppHeader
+    }
 };
 </script>
 
 <!-- HTML -->
 <template>
-  
+<!-- APPHEADER -->
+  <AppHeader/>
 </template>
 
 <!-- CSS -->
